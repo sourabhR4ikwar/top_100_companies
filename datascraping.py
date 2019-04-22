92be9f5cb9af11b068ae14d18912d3832cffed43
sourabhR4ikwar <sourabh.d.raikwar@gmail.com>
2019-04-22 18:51:14 +0530

    Add program.py

---
+++ datascraping.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+
+df = pd.read_excel("Excel_Files/Data_from_statista.xlsx","Sheet1")
+userAgent = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.103 Safari/537.36'}
+for x in range(0,len(df.index)):
+    soup = BeautifulSoup(requests.get("https://www.google.com/search?q="+df.iloc[x, 0]+"+headquarters",headers=userAgent).text, "html.parser")
+    headquarters = soup.find("div",{"class":"Z0LcW"}).get_text()
+    soup = BeautifulSoup(requests.get("https://www.google.com/search?q="+headquarters+"+coordinates",headers=userAgent).text,"html.parser")
+    location = soup.find("div", {"class":"Z0LcW"}).get_text()
+    df.loc[x, "HeadQuarters"] = headquarters
+    df.loc[x, "Location"] = location
+    print(df.iloc[x, 0],"|",headquarters,"|", location)
+    df.to_excel("Output.xlsx")
